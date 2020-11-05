e142b04313905d7efa86ecc000b2c861fbd5137e
Alexey Borisovets <alexey.borisovets@mail.ru>
2020-11-05 18:04:21 +0300

    add Orders/Index view

--- Warehouse/Controllers/OrdersController.cs
+++ Warehouse/Controllers/OrdersController.cs
@@ -6,17 +6,38 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
+using Warehouse.Models;
+using Warehouse.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Warehouse.Controllers
 {
     [Authorize]
     public class OrdersController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public OrdersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         // GET: Orders
         [Authorize]
         public ActionResult Index()
         {
-            return View();
+            var items = new OrderItem[]{
+                new OrderItem{ OrderItemId = 1, Price = 100, Product = _context.Products.Include(p => p.Pictures).Include(p => p.Unit)
+                    .Include(p => p.ManufactureCountry).FirstOrDefault(i=>i.ProductId == 1)},
+                new OrderItem{ OrderItemId = 2, Price = 102, Product = _context.Products.Include(p => p.Pictures).Include(p => p.Unit)
+                    .Include(p => p.ManufactureCountry).FirstOrDefault(i=>i.ProductId == 2)},
+
+            };
+            var resultPrice = items.Sum(i => i.Price);
+
+
+            var l = new List<Order>();
+            l.Add(new Order { OrderDate = DateTime.Now, OrderId = 1, UserId = 1, OrderStatus = new OrderStatus { OrderStatusId = 1 }, TotalPrice = resultPrice, Items = items.ToList() });
+            return View(l);
         }
 
         // GET: Orders/Details/5
