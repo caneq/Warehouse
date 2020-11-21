58314959448507b33032e94ea3abbb71c73e4e78
Korobkin Alexander <korobkin.alex2000@yandex.ru>
2020-11-21 17:00:16 +0300

    add repository

---
+++ Warehouse.DataAccesLayer/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warehouse.DataAccesLayer.Interfaces
+{
+    interface IRepository<T> where T : class
+    {
+        void CreateAsync(T item);
+        void UpdateAsync(T item);
+        Task<T> ReadAsync(int id);
+        IEnumerable<T> Read(Func<T, Boolean> predicate);
+        void DeleteAsync(T item);
+    }
+}
---
+++ Warehouse.DataAccesLayer/Repositories/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Warehouse.DataAccesLayer.Interfaces;
+
+namespace Warehouse.DataAccesLayer.Repositories
+{
+    class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly DbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(DbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+        public async void CreateAsync(T item)
+        {
+            await _dbSet.AddAsync(item);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<T> ReadAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public IEnumerable<T> Read(Func<T, bool> predicate)
+        {
+            return _dbSet.AsNoTracking().Where(predicate).ToList();
+        }
+
+        public async void UpdateAsync(T item)
+        {
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+
+        public async void DeleteAsync(T item)
+        {
+            _dbSet.Remove(item);
+            await _context.SaveChangesAsync();
+        }
+
+    }
+}
