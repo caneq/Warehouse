17bead2068581b1ee4eeb73a065f533714f39469
Borisovets Alexey <alexey.borisovets@mail.ru>
2020-11-30 00:24:07 +0300

    add client side model validating

--- Warehouse/ViewModels/ProductViewModel.cs
+++ Warehouse/ViewModels/ProductViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Warehouse.ClassLibrary;
@@ -9,15 +10,35 @@ namespace Warehouse.ViewModels
     public class ProductViewModel
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Введите имя продукта")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Длина строки должна быть от 3 до 50 символов")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Выберите единицу измерения")]
         public int UnitId { get; set; }
         public UnitViewModel Unit { get; set; }
-        public string Description { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = "Введите вес")]
         public float Weight { get; set; }
-        public Price Price { get; set; }
+
+        [Required(ErrorMessage = "Введите цену")]
+        [RegularExpression("^[0-9]+(\\.[0-9]{1,2})?$", ErrorMessage ="Некорректная цена")]
+        public string Price { get; set; }
+
+        [Required(ErrorMessage = "Введите срок годности")]
+        [Range(0, int.MaxValue, ErrorMessage = "Некорректное число")]
         public int ShelfLife { get; set; }
+
+        [Required(ErrorMessage = "Выберите страну происхождения")]
         public int ManufactureCountryId { get; set; }
         public CountryViewModel ManufactureCountry { get; set; }
+
+        [Required(ErrorMessage = "Введите количество на складе")]
+        [Range(0,int.MaxValue, ErrorMessage = "Некорректное число")]
         public int CountInStock { get; set; }
         public List<UrlViewModel> Pictures { get; set; }
     }
