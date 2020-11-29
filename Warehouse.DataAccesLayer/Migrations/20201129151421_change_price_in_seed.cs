f2d31d48d669c120d8f4c490231f89791b0b1303
Borisovets Alexey <alexey.borisovets@mail.ru>
2020-11-29 18:15:01 +0300

    update seeding price

---
+++ Warehouse.DataAccesLayer/Migrations/20201129151421_change_price_in_seed.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Warehouse.DataAccessLayer.Migrations
+{
+    public partial class change_price_in_seed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 27000L);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Price",
+                value: 64600L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Price",
+                value: 270L);
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Price",
+                value: 646L);
+        }
+    }
+}
