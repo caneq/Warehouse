b7e1063b010193a5d3990442fda0beea9d0973f9
Korobkin Alexander <korobkin.alex2000@yandex.ru>
2020-12-08 22:27:51 +0300

    add ordering in OrdersRepo, hide button on order details page

--- Warehouse.DataAccesLayer/Repositories/OrdersRepository.cs
+++ Warehouse.DataAccesLayer/Repositories/OrdersRepository.cs
@@ -46,6 +46,7 @@ namespace Warehouse.DataAccessLayer.Repositories
                         .ThenInclude(p => p.Unit)
                 .FirstOrDefaultAsync(predicate);
 
+            c.OrderStatuses = c.OrderStatuses.OrderByDescending(c => c.DateTime).ToList();
             return c;
         }
         public IEnumerable<Order> ReadMany(Func<Order, bool> predicate)
