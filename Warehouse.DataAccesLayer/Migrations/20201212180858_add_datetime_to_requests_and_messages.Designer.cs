abc83b52f5f1e5e30af7bc99bacfe55e998db1df
Borisovets Alexey <alexey.borisovets@mail.ru>
2020-12-12 21:09:41 +0300

    add migration for datetimes

--- Warehouse.DataAccesLayer/Migrations/20201212180858_add_datetime_to_requests_and_messages.Designer.cs
+++ Warehouse.DataAccesLayer/Migrations/20201212180858_add_datetime_to_requests_and_messages.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Warehouse.DataAccessLayer.Data;
 
 namespace Warehouse.DataAccessLayer.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201212180858_add_datetime_to_requests_and_messages")]
+    partial class add_datetime_to_requests_and_messages
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
