0a512757ef79017cec95cba8291cd1d753aee2cc
Borisovets Alexey <alexey.borisovets@mail.ru>
2020-12-12 18:56:33 +0300

    add migration

---
+++ Warehouse.DataAccesLayer/Migrations/20201212155543_add_client_requests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Warehouse.DataAccessLayer.Migrations
+{
+    public partial class add_client_requests : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ClientRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApplicationUserId = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true),
+                    Completed = table.Column<bool>(nullable: false),
+                    ClientUnreadMessagesCount = table.Column<int>(nullable: false),
+                    ManagersUnreadMessagesCount = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ClientRequests_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ClientRequestMessage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClientRequestId = table.Column<int>(nullable: false),
+                    ApplicationUserId = table.Column<string>(nullable: true),
+                    MessageText = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ClientRequestMessage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ClientRequestMessage_AspNetUsers_ApplicationUserId",
+                        column: x => x.ApplicationUserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ClientRequestMessage_ClientRequests_ClientRequestId",
+                        column: x => x.ClientRequestId,
+                        principalTable: "ClientRequests",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientRequestMessage_ApplicationUserId",
+                table: "ClientRequestMessage",
+                column: "ApplicationUserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientRequestMessage_ClientRequestId",
+                table: "ClientRequestMessage",
+                column: "ClientRequestId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ClientRequests_ApplicationUserId",
+                table: "ClientRequests",
+                column: "ApplicationUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ClientRequestMessage");
+
+            migrationBuilder.DropTable(
+                name: "ClientRequests");
+        }
+    }
+}
