0d15736d7d13443b42097242f11d8960d9334789
Borisovets Alexey <alexey.borisovets@mail.ru>
2020-12-12 20:31:40 +0300

    add updating ClientRequests

--- Warehouse.DataAccesLayer/Repositories/ClientRequestRepository.cs
+++ Warehouse.DataAccesLayer/Repositories/ClientRequestRepository.cs
@@ -55,8 +55,25 @@ namespace Warehouse.DataAccessLayer.Repositories
 
         public async Task UpdateAsync(ClientRequest item)
         {
-            _context.Entry(item).State = EntityState.Modified;
-            _context.Update(item);
+            var req = await _dbSet
+                .Include(c => c.ApplicationUser)
+                .Include(c => c.Messages)
+                    .ThenInclude(c => c.User)
+                .FirstOrDefaultAsync(r => r.Id == item.Id);
+
+            req.ApplicationUserId = item.ApplicationUserId;
+            req.ClientUnreadMessagesCount = item.ClientUnreadMessagesCount;
+            req.Completed = item.Completed;
+            req.ManagersUnreadMessagesCount = item.ManagersUnreadMessagesCount;
+
+            var added = item.Messages.Where(s => !req.Messages.Any(o => o.Id == s.Id));
+            var removed = req.Messages.Where(s => !item.Messages.Any(o => o.Id == s.Id));
+
+            req.Messages.RemoveAll(m => removed.Contains(m));
+            req.Messages.AddRange(added);
+
+            req.Title = item.Title;
+            
             await _context.SaveChangesAsync();
         }
     }
