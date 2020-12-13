26be2bbcc41c3c2da6cb543474291b4fbbe3bc8d
Korobkin Alexander <korobkin.alex2000@yandex.ru>
2020-12-13 23:34:08 +0300

    add DTOs

---
+++ Warehouse.BusinessLogicLayer/DataTransferObjects/SupplierOrderItemDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Warehouse.BusinessLogicLayer.DataTransferObjects;
+using Warehouse.ClassLibrary;
+
+namespace Warehouse.BusinessLogicLayer.DataTransferObjects
+{
+    public class SupplierOrderItemDTO
+    {
+        public int Id { get; set; }
+        public int SupplierOrderId { get; set; }
+        public Price Price { get; set; }
+        public int ProductId { get; set; }
+        public ProductDTO Product { get; set; }
+    }
+}
