0bbc4be3489a95d4eac4ffdee2110ec27fd1d52b
Korobkin Alexander <korobkin.alex2000@yandex.ru>
2020-12-24 22:30:46 +0300

    add Supplier views and controller

---
+++ Warehouse/Controllers/SupplierController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Warehouse.BusinessLogicLayer.DataTransferObjects;
+using Warehouse.BusinessLogicLayer.Interfaces;
+using Warehouse.ViewModels;
+
+namespace Warehouse.Controllers
+{
+    public class SupplierController : Controller
+    {
+        private readonly ISupplierService _supplierService;
+        private readonly IMapper _mapper;
+        public SupplierController(ISupplierService supplierService, IMapper mapper)
+        {
+            _supplierService = supplierService;
+            _mapper = mapper;
+        }
+        public ActionResult Index()
+        {
+            var suppliers = _mapper.Map<IEnumerable<SupplierViewModel>>(_supplierService.ReadAll());
+            return View(suppliers);
+        }
+
+        public async Task<ActionResult> Details(int id)
+        {
+            var supplier = _mapper.Map<SupplierViewModel>(await _supplierService.ReadAsync(id));
+            return View(supplier);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(SupplierDTO supplier)
+        {
+            try
+            {
+                await _supplierService.CreateAsync(supplier);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Edit(int id)
+        {
+            var supplier = _mapper.Map<SupplierViewModel>(await _supplierService.ReadAsync(id));
+            return View(supplier);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(SupplierDTO supplier)
+        {
+            try
+            {
+                await _supplierService.UpdateAsync(supplier);
+
+                return RedirectToAction(nameof(Details), new { id = supplier.Id });
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public async Task<ActionResult> Delete(int id)
+        {
+            var sup = _mapper.Map<SupplierViewModel>(await _supplierService.ReadAsync(id));
+            return View(sup);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                var sup = await _supplierService.ReadAsync(id);
+                if (sup == null) return NotFound();
+
+                await _supplierService.DeleteAsync(sup);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return RedirectToAction(nameof(Details), new { id });
+            }
+        }
+    }
+}
--- Warehouse/Controllers/SupplierOrdersController.cs
+++ Warehouse/Controllers/SupplierOrdersController.cs
@@ -10,72 +10,44 @@ using Warehouse.DataAccessLayer.Data;
 using AutoMapper;
 using Warehouse.ViewModels;
 using Warehouse.ClassLibrary;
+using Warehouse.BusinessLogicLayer.Interfaces;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Warehouse.BusinessLogicLayer.Models;
 
 namespace Warehouse.Controllers
 {
     public class SupplierOrdersController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private readonly ISupplierService _supplierService;
+        private readonly ISupplierOrderService _supplierOrderService;
+        private readonly IProductService _productService;
         private readonly IMapper _mapper;
-        public SupplierOrdersController(ApplicationDbContext context, IMapper mapper)
+        public SupplierOrdersController(IMapper mapper, ISupplierService supplierService,
+            ISupplierOrderService supplierOrderService, IProductService productService)
         {
-            _context = context;
             _mapper = mapper;
+            _supplierService = supplierService;
+            _supplierOrderService = supplierOrderService;
+            _productService = productService;
         }
-        // GET: SupplierOrders
         public ActionResult Index()
         {
-            //var items = new OrderItem[]{
-            //    new OrderItem{ Id = 1, Price = new Price(100), Product = _context.Products.Include(p => p.Pictures).Include(p => p.Unit)
-            //        .Include(p => p.ManufactureCountry).FirstOrDefault(i=>i.Id == 3)},
-            //    new OrderItem{ Id = 2, Price = new Price(102), Product = _context.Products.Include(p => p.Pictures).Include(p => p.Unit)
-            //        .Include(p => p.ManufactureCountry).FirstOrDefault(i=>i.Id == 4)},
-
-            //};
-            //var resultPrice = new Price(items.Sum(i => i.Price.Penny));
-
-
-            //var l = new List<Order>();
-            //l.Add(new Order { OrderDate = DateTime.Now, Id = 2, UserId = "123", OrderStatus = _context.OrderStatuses.Find(1), TotalPrice = resultPrice, Items = items.ToList() });
-
-            //items = new OrderItem[]{
-            //    new OrderItem{ Id = 3, Price = new Price(399999), Product = _context.Products.Include(p => p.Pictures).Include(p => p.Unit)
-            //        .Include(p => p.ManufactureCountry).FirstOrDefault(i=>i.Id == 5)},
-            //    new OrderItem{ Id = 4, Price = new Price(26099), Product = _context.Products.Include(p => p.Pictures).Include(p => p.Unit)
-            //        .Include(p => p.ManufactureCountry).FirstOrDefault(i=>i.Id == 1)},
-
-            //};
-            //resultPrice = new Price(items.Sum(i => i.Price.Penny));
-
-            //l.Add(new Order { OrderDate = DateTime.Today, Id = 1, UserId = "123", OrderStatus = _context.OrderStatuses.Find(3), TotalPrice = resultPrice, Items = items.ToList() });
-            //return View(_mapper.Map<IEnumerable<OrderViewModel>>(l));
             return View();
         }
 
-        // GET: SupplierOrders/Details/5
         public async Task<ActionResult> Details(int id)
         {
-            //var items = new OrderItem[]{
-            //    new OrderItem{ Id = 1, Price = new Price(10000), Product = _context.Products.Include(p => p.Pictures).Include(p => p.Unit)
-            //        .Include(p => p.ManufactureCountry).FirstOrDefault(i=>i.Id == 3)},
-            //    new OrderItem{ Id = 2, Price = new Price(10200), Product = _context.Products.Include(p => p.Pictures).Include(p => p.Unit)
-            //        .Include(p => p.ManufactureCountry).FirstOrDefault(i=>i.Id == 4)},
-
-            //};
-            //var resultPrice = new Price(items.Sum(i => i.Price.Penny));
-
-            //Order o = new Order { OrderDate = DateTime.Now, Id = 2, UserId = "123", OrderStatus = _context.OrderStatuses.Find(1), TotalPrice = resultPrice, Items = items.ToList() };
-            //return View(_mapper.Map<OrderViewModel>(o));
             return View();
         }
 
-        // GET: SupplierOrders/Create
         public ActionResult Create()
         {
+            var defSelect = new SelectListItem { Value = "-1", Text = "Не выбрано", Selected = true };
+            ViewBag.Suppliers = (new SelectList(_supplierService.ReadAll(), "Id", "Name")).Append(defSelect);
+            ViewBag.Products = (new SelectList(_productService.ReadMany(new ProductFilterParams()), "Id", "Name")).Append(defSelect);
             return View();
         }
 
-        // POST: SupplierOrders/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create(IFormCollection collection)
@@ -92,13 +64,11 @@ namespace Warehouse.Controllers
             }
         }
 
-        // GET: SupplierOrders/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
         }
 
-        // POST: SupplierOrders/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit(int id, IFormCollection collection)
@@ -115,13 +85,11 @@ namespace Warehouse.Controllers
             }
         }
 
-        // GET: SupplierOrders/Delete/5
         public ActionResult Delete(int id)
         {
             return View();
         }
 
-        // POST: SupplierOrders/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Delete(int id, IFormCollection collection)
